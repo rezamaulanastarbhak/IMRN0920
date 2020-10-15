badce9626f0a6a27c2707c5e0445414cf7cd5cb3
rezamaulanastarbhak <rezamaulanastarbhak@gmail.com>
2020-10-16 06:35:38 +0700

    Tugas 14

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -2,15 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import YoutubeUI from './Tugas/Tugas12/App'
-import AboutScreen from './Tugas/Tugas13/AboutMe';
-import LoginScreen from './Tugas/Tugas13/LoginScreen';
-import Register from './Tugas/Tugas13/Register';
+// import YoutubeUI from './Tugas/Tugas12/App'
+// import AboutScreen from './Tugas/Tugas13/AboutMe';
+// import LoginScreen from './Tugas/Tugas13/LoginScreen';
+// import Register from './Tugas/Tugas13/Register';
+import Main from './Tugas/Tugas14/Main';
 
 export default function App() {
   return (
     
-    <Register/>
+    <Main/>
     
   );
 }
